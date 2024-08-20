28a6df5ef28ab2f5ce0d2176751209ee3e9122dc
sergioBustamante <sergio.bustamante@niceincontact.com>
2024-08-19 21:42:11 -0400

    Saludador con genero y edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,19 @@
 function greet() {
   const name = document.getElementById('name').value.trim();
+  const gender = document.getElementById('gender').value;
+  const age = document.getElementById('age').value.trim();
+
   if (name) {
-      const greeting = `Hola ${name}!`;
+      let greeting = `Hola ${name}`;
+      if (gender) {
+          greeting += gender === 'masculino' ? ', señor' : ', señora';
+      }
+      if (age) {
+          greeting += `, tienes ${age} años`;
+      }
+      greeting += '!';
       document.getElementById('greetingMessage').textContent = greeting;
+  } else {
+      document.getElementById('greetingMessage').textContent = "Por favor, ingresa tu nombre.";
   }
 }
