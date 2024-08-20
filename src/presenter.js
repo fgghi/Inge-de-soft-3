22287c68bfe220c50aec9fdf504fa5b60be22624
sergioBustamante <sergio.bustamante@niceincontact.com>
2024-08-19 21:35:18 -0400

    Saludador con nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,7 @@
 function greet() {
-  greeting = 'Hola';
-  alert(greeting);
+  const name = document.getElementById('name').value.trim();
+  if (name) {
+      const greeting = `Hola ${name}!`;
+      document.getElementById('greetingMessage').textContent = greeting;
+  }
 }
