c0c0131c0f2e8848e3298aa76379601e78daa565
sergioBustamante <sergio.bustamante@niceincontact.com>
2024-08-19 21:44:49 -0400

    Saludador con lenguaje español e ingles

--- src/presenter.js
+++ src/presenter.js
@@ -2,18 +2,32 @@ function greet() {
   const name = document.getElementById('name').value.trim();
   const gender = document.getElementById('gender').value;
   const age = document.getElementById('age').value.trim();
+  const language = document.getElementById('language').value;
 
   if (name) {
-      let greeting = `Hola ${name}`;
-      if (gender) {
-          greeting += gender === 'masculino' ? ', señor' : ', señora';
+      let greeting = '';
+      if (language === 'es') {
+          greeting = `Hola ${name}`;
+          if (gender) {
+              greeting += gender === 'masculino' ? ', señor' : ', señora';
+          }
+          if (age) {
+              greeting += `, tienes ${age} años`;
+          }
+          greeting += '!';
+      } else if (language === 'en') {
+          greeting = `Hello ${name}`;
+          if (gender) {
+              greeting += gender === 'masculino' ? ', sir' : ', madame';
+          }
+          if (age) {
+              greeting += `, you are ${age} years old`;
+          }
+          greeting += '!';
       }
-      if (age) {
-          greeting += `, tienes ${age} años`;
-      }
-      greeting += '!';
+
       document.getElementById('greetingMessage').textContent = greeting;
   } else {
-      document.getElementById('greetingMessage').textContent = "Por favor, ingresa tu nombre.";
+      document.getElementById('greetingMessage').textContent = language === 'es' ? "Por favor, ingresa tu nombre." : "Please enter your name.";
   }
 }
